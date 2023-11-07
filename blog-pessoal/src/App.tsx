8049ff718fd156cbcd61086bd999ed711a680bdb
Pedro Melo <pedromarcosprado26@gmail.com>
2023-11-07 14:58:09 -0300

    feat: CRUD postagem

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -9,6 +9,9 @@ import Navbar from './components/navbar/Navbar';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
   return (
@@ -22,10 +25,16 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- blog-pessoal/src/pages/home/Home.tsx
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -1,11 +1,7 @@
-import { useContext } from "react"
-import { AuthContext } from "../../context/AuthContext"
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem"
 
 function Home() {
-
-    // const { nome } = useContext(AuthContext)
-
-
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -19,10 +15,8 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                            >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -36,6 +30,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <ListaPostagens />
         </>
     )
 }
